9b36f9857ee5186056d992d4d34ca5bda5348770
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-01-25 16:37:01 +0100

    change the app side bare

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,76 +1,79 @@
-import { Home, Settings, List , UserPlus } from "lucide-react";
-
+import {
+  Calendar,
+  ClipboardList,
+  FileText,
+  LayoutDashboard,
+  Settings,
+  UserCog,
+  FilePlus , 
+} from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarHeader,
+  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { Link } from "react-router-dom";
 
-// Menu items.
-const items = [
+const menuItems = [
   {
-    title: "Accueil",
+    title: "Tableau de bord",
+    icon: LayoutDashboard,
     url: "/",
-    icon: Home,
   },
   {
-    title: "Demande de congés",
-    url: "demande-conges",
-    icon: Home,
+    title: "cree un congé",
+    icon: FilePlus,
+    url: "creer-conge",
   },
   {
-    title: "Fonctionnaires",
-    url: "ajouter-fonctionnaires",
-    icon: UserPlus,
+    title: "Demandes de congé",
+    icon: ClipboardList,
+    url: "demandes-de-conge",
   },
   {
-    title: "liste des demandes",
-    url: "liste-demands",
-    icon: List,
+    title: "Solde de congé",
+    icon: Calendar,
+    url: "solde-de-conge",
   },
   {
-    title: "Settings",
-    url: "settings",
+    title: "Rapports",
+    icon: FileText,
+    url: "rapports",
+  },
+  {
+    title: "Panneau d'administration",
+    icon: UserCog,
+    url: "admin",
+  },
+  {
+    title: "Paramètres",
     icon: Settings,
+    url: "parametres",
   },
-  {
-    title: "Deconnexion",
-    url: "deconnexion",
-    icon: Home,
-  }
 ];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-              <h2>SGRH</h2>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
+          <SidebarGroupLabel>Leave Management</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
+              {menuItems.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <Link to={item.url}>
-                      <item.icon />
+                    <a href={item.url} className="flex items-center gap-2">
+                      <item.icon className="h-4 w-4" />
                       <span>{item.title}</span>
-                    </Link>
+                    </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
-             
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
